72fdad6e246d0444d419c05fb797341f3b8c236b
Sourith2008 <sankardey1121990@gmail.com>
2026-08-21 22:39:42 +0530

    Quantized Qwen 2.5

--- app/model.py
+++ app/model.py
@@ -1,11 +1,21 @@
 from sentence_transformers import SentenceTransformer,CrossEncoder
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM,BitsAndBytesConfig
 import torch
 import streamlit as st
 
+quant_config=BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True
+)
+
 @st.cache_resource
 def load_model():
-    model=AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B-Instruct")
+    model=AutoModelForCausalLM.from_pretrained(
+        "Qwen/Qwen2.5-0.5B-Instruct",
+        quantization_config=quant_config
+        )
     tokenizer=AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B-Instruct")
 
     embed_model=SentenceTransformer("all-MiniLM-L6-v2")
